74a811985a5bcd0c67684641435c1ebce7224323
Gabor Sar <gabor.sar@rightster.com>
2014-04-04 17:34:09 +0100

    Replace jQuery with AngularJS

--- script.js
+++ script.js
@@ -1,35 +1,129 @@
-$(document).ready(function () {
+'use strict';
 
-    'use strict';
 
-    var $response = $('.response'),
-        $forms = $('.forms'),
-        appendResponse;
 
-    appendResponse = function (jqXHR, css) {
-        var json, html;
-        json = JSON.stringify(jqXHR.responseJSON, null, '  ');
-        html = '<div class="' + css + '">'
-            + jqXHR.status + ' : '
-            + json.replace(/\n/g, '<br>').replace(/\s/g, '&nbsp;')
-            + '</div>';
-        $response.prepend(html);
+var app = angular.module('app', []);
+
+
+
+app.service('formService', function () {
+
+    var self = this;
+
+    this.list = angular.fromJson(localStorage.forms) || [];
+
+    this.add = function (params) {
+        self.list.push({
+            url: params.url || '/',
+            method: params.method || 'get',
+            label: params.label || params.url || '/',
+            fields: params.fields || [],
+            data: {}
+        });
+        localStorage.forms = angular.toJson(self.list);
     };
 
-    $forms.find('form').each(function () {
-        var $form = $(this);
-        $form.on('submit', function (event) {
-            event.preventDefault();
-            $.ajax({
-                type: $form.attr('method').toUpperCase(),
-                url: $forms.data('url') + $form.attr('action'),
-                crossDomain: true,
-                data: $form.serialize()
-            }).done(function (data, textStatus, jqXHR) {
-                appendResponse(jqXHR, 'done');
-            }).fail(function (jqXHR) {
-                appendResponse(jqXHR, 'fail');
-            });
-        });
-    });
+    this.setList = function (list) {
+        this.list = angular.fromJson(list);
+        localStorage.forms = list;
+    };
 });
+
+
+
+app.service('responseService', function () {
+
+    var self = this;
+
+    this.list = angular.fromJson(localStorage.responses) || [];
+
+    this.remove = function (index) {
+        this.list.splice(index, 1);
+        localStorage.responses = angular.toJson(self.list);
+    };
+
+    this.addSuccess = function (data, status) {
+        self.list.unshift({
+            status: status,
+            data: data,
+            error: false
+        });
+        localStorage.responses = angular.toJson(self.list);
+    };
+
+    this.addError = function (data, status) {
+        self.list.unshift({
+            status: status,
+            data: data,
+            error: true
+        });
+        localStorage.responses = angular.toJson(self.list);
+    };
+});
+
+
+
+app.controller('FormListController', [
+
+    '$scope',
+    '$filter',
+    'formService',
+
+    function ($scope, $filter, formService) {
+
+        $scope.forms = formService.list;
+
+        $scope.editor = {
+            show: false,
+            content: $filter('json')(formService.list)
+        };
+
+        $scope.toggleEditor = function () {
+            $scope.editor.show = !$scope.editor.show;
+        };
+
+        $scope.saveForms = function () {
+            formService.setList($scope.editor.content);
+            location.reload();
+        };
+    }
+]);
+
+
+
+app.controller('FormController', [
+
+    '$scope',
+    '$http',
+    'responseService',
+
+    function ($scope, $http, responseService) {
+
+        $scope.sendRequest = function () {
+            var request = $http({
+                url: $scope.form.url,
+                method: $scope.form.method,
+                params: $scope.form.data
+            });
+            request.success(responseService.addSuccess);
+            request.error(responseService.addError);
+        };
+    }
+]);
+
+
+
+app.controller('ResponseListController', [
+
+    '$scope',
+    'responseService',
+
+    function ($scope, responseService) {
+
+        $scope.responses = responseService.list;
+
+        $scope.remove = function (index) {
+            responseService.remove(index);
+        };
+    }
+]);
