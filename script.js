4eaa12324105969f8f48ed747df7d8ab84657086
Gabor Sar <gabor.sar@gmail.com>
2014-04-06 12:21:28 +0100

    code cleaning

--- script.js
+++ script.js
@@ -1,118 +1,165 @@
 'use strict';
 
-
-
 var app = angular.module('app', []);
 
-
-
-app.service('formService', function () {
+/**
+ * Forms services.
+ */
+app.service('Forms', function () {
 
     var self = this;
 
+    // restore the list of forms from the localStorage
     this.list = angular.fromJson(localStorage.forms) || [];
 
-    this.add = function (params) {
-        self.list.push({
-            url: params.url || '/',
-            method: params.method || 'get',
-            label: params.label || params.url || '/',
-            fields: params.fields || [],
-            data: {}
-        });
+    /**
+     * Updates the localStorage copy.
+     * @api private
+     */
+    var updateLocalStorage = function () {
         localStorage.forms = angular.toJson(self.list);
     };
 
+    /**
+     * Overwrites the list of forms.
+     * @param {string} list JSON
+     * @returns {Array}
+     * @api public
+     */
     this.setList = function (list) {
         this.list = angular.fromJson(list);
-        localStorage.forms = list;
+        updateLocalStorage();
+        return this.list;
     };
 });
 
-
-
-app.service('responseService', function () {
+/**
+ * Responses service.
+ */
+app.service('Responses', function () {
 
     var self = this;
 
+    // restore the list of forms from the localStorage
     this.list = angular.fromJson(localStorage.responses) || [];
 
-    this.remove = function (index) {
-        this.list.splice(index, 1);
+    /**
+     * Updates the localStorage copy.
+     * @api private
+     */
+    var updateLocalStorage = function () {
         localStorage.responses = angular.toJson(self.list);
     };
 
+    /**
+     * Removes a response by it's index.
+     * @param {Number} index
+     * @api public
+     */
+    this.remove = function (index) {
+        this.list.splice(index, 1);
+        updateLocalStorage();
+    };
+
+    /**
+     * Adds a success response to the list.
+     * @param {Object} data
+     * @param {Number} status
+     * @api public
+     */
     this.addSuccess = function (data, status) {
         self.list.unshift({
             status: status,
             data: data,
             error: false
         });
-        localStorage.responses = angular.toJson(self.list);
+        updateLocalStorage();
     };
 
+    /**
+     * Adds an error response to the list.
+     * @param {Object} data
+     * @param {Number} status
+     * @api public
+     */
     this.addError = function (data, status) {
         self.list.unshift({
             status: status,
             data: data,
             error: true
         });
-        localStorage.responses = angular.toJson(self.list);
+        updateLocalStorage();
     };
 });
 
-
-
+/**
+ * Form list controller.
+ */
 app.controller('FormListController', [
 
     '$scope',
     '$filter',
-    'formService',
+    'Forms',
 
-    function ($scope, $filter, formService) {
+    function ($scope, $filter, Forms) {
 
-        $scope.forms = formService.list;
+        // list of forms
+        $scope.forms = Forms.list;
 
+        // form list editor model
         $scope.editor = {
             show: false,
-            content: $filter('json')(formService.list)
+            content: $filter('json')($scope.forms)
         };
 
+        /**
+         * Toggles the form list editor form.
+         * @api public
+         */
         $scope.toggleEditor = function () {
             $scope.editor.show = !$scope.editor.show;
         };
 
+        /**
+         * Save the form list editor form.
+         * @api public
+         */
         $scope.saveForms = function () {
-            formService.setList($scope.editor.content);
-            location.reload();
+            this.forms = Forms.setList($scope.editor.content);
         };
     }
 ]);
 
-
-
+/**
+ * Form list item controller.
+ */
 app.controller('FormController', [
 
     '$scope',
     '$http',
-    'responseService',
+    'Responses',
 
-    function ($scope, $http, responseService) {
+    function ($scope, $http, Responses) {
 
+        /**
+         * Sends an API request.
+         * @api public
+         */
         $scope.sendRequest = function () {
             var request = $http({
                 url: $scope.form.url,
                 method: $scope.form.method,
                 params: $scope.form.data
             });
-            request.success(responseService.addSuccess);
-            request.error(responseService.addError);
+            request.success(Responses.addSuccess);
+            request.error(Responses.addError);
         };
     }
 ]);
 
-
-
+/**
+ * Response list controller.
+ */
 app.controller('ResponseListController', [
 
     '$scope',
@@ -120,8 +167,14 @@ app.controller('ResponseListController', [
 
     function ($scope, responseService) {
 
+        // list of responses
         $scope.responses = responseService.list;
 
+        /**
+         * Removes a response by it's index.
+         * @param {Number} index
+         * @api public
+         */
         $scope.remove = function (index) {
             responseService.remove(index);
         };
