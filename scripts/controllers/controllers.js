10fbaf44791fed27a30e7649d88c338f67cfabfe
Gabor Sar <gabor.sar@rightster.com>
2014-04-07 09:32:55 +0100

    Restructuring code

---
+++ scripts/controllers/controllers.js
@@ -0,0 +1,107 @@
+'use strict';
+
+/**
+ * Application.
+ * @module apitester
+ */
+var app = angular.module('apitester', [ 'apitester.services' ]);
+
+
+/**
+ * Form list controller.
+ */
+app.controller('FormsCtrl', [
+
+    '$scope',
+    '$filter',
+    'Forms',
+
+    function ($scope, $filter, Forms) {
+
+        // list of forms
+        $scope.forms = Forms.list;
+
+        // form list editor model
+        $scope.editor = {
+            show: false,
+            content: $filter('json')($scope.forms)
+        };
+
+        /**
+         * Toggles the form list editor form.
+         * @api public
+         */
+        $scope.toggleEditor = function () {
+            $scope.editor.show = !$scope.editor.show;
+        };
+
+        /**
+         * Save the form list editor form.
+         * @api public
+         */
+        $scope.saveForms = function () {
+            this.forms = Forms.setList($scope.editor.content);
+        };
+    }
+]);
+
+/**
+ * Form list item controller.
+ */
+app.controller('FormCtrl', [
+
+    '$scope',
+    '$http',
+    'Responses',
+
+    function ($scope, $http, Responses) {
+
+        /**
+         * Sends an API request.
+         * @api public
+         */
+        $scope.sendRequest = function () {
+            var request = $http({
+                url: $scope.form.url,
+                method: $scope.form.method,
+                params: $scope.form.data
+            });
+            request.success(Responses.addSuccess);
+            request.error(Responses.addError);
+        };
+    }
+]);
+
+/**
+ * Response list controller.
+ */
+app.controller('ResponsesCtrl', [
+
+    '$scope',
+    '$http',
+    'Responses',
+
+    function ($scope, $http, Responses) {
+
+        // list of responses
+        $scope.responses = Responses.list;
+
+        /**
+         * Returns loading state.
+         * @returns {Boolean}
+         * @api public
+         */
+        $scope.isLoading = function () {
+            return $http.pendingRequests.length > 0;
+        };
+
+        /**
+         * Removes a response by it's index.
+         * @param {Number} index
+         * @api public
+         */
+        $scope.remove = function (index) {
+            Responses.remove(index);
+        };
+    }
+]);
--- scripts/services.js
+++ scripts/services.js
@@ -1,15 +1,15 @@
 'use strict';
 
 /**
- * Applciations
- * @module app
+ * Services.
+ * @module apitester/services
  */
-var app = angular.module('app', []);
+var services = angular.module('apitester.services', []);
 
 /**
- * Forms services.
+ * Forms service.
  */
-app.service('Forms', function () {
+services.service('Forms', function () {
 
     var self = this, demoForms, updateLocalStorage;
 
@@ -63,7 +63,7 @@ app.service('Forms', function () {
 /**
  * Responses service.
  */
-app.service('Responses', function () {
+services.service('Responses', function () {
 
     var self = this, updateLocalStorage;
 
@@ -118,102 +118,3 @@ app.service('Responses', function () {
         updateLocalStorage();
     };
 });
-
-/**
- * Form list controller.
- */
-app.controller('FormListController', [
-
-    '$scope',
-    '$filter',
-    'Forms',
-
-    function ($scope, $filter, Forms) {
-
-        // list of forms
-        $scope.forms = Forms.list;
-
-        // form list editor model
-        $scope.editor = {
-            show: false,
-            content: $filter('json')($scope.forms)
-        };
-
-        /**
-         * Toggles the form list editor form.
-         * @api public
-         */
-        $scope.toggleEditor = function () {
-            $scope.editor.show = !$scope.editor.show;
-        };
-
-        /**
-         * Save the form list editor form.
-         * @api public
-         */
-        $scope.saveForms = function () {
-            this.forms = Forms.setList($scope.editor.content);
-        };
-    }
-]);
-
-/**
- * Form list item controller.
- */
-app.controller('FormController', [
-
-    '$scope',
-    '$http',
-    'Responses',
-
-    function ($scope, $http, Responses) {
-
-        /**
-         * Sends an API request.
-         * @api public
-         */
-        $scope.sendRequest = function () {
-            var request = $http({
-                url: $scope.form.url,
-                method: $scope.form.method,
-                params: $scope.form.data
-            });
-            request.success(Responses.addSuccess);
-            request.error(Responses.addError);
-        };
-    }
-]);
-
-/**
- * Response list controller.
- */
-app.controller('ResponseListController', [
-
-    '$scope',
-    '$http',
-    'Responses',
-
-    function ($scope, $http, Responses) {
-
-        // list of responses
-        $scope.responses = Responses.list;
-
-        /**
-         * Returns loading state.
-         * @returns {Boolean}
-         * @api public
-         */
-        $scope.isLoading = function () {
-            return $http.pendingRequests.length > 0;
-        };
-
-        /**
-         * Removes a response by it's index.
-         * @param {Number} index
-         * @api public
-         */
-        $scope.remove = function (index) {
-            Responses.remove(index);
-        };
-    }
-]);
